143a1dac3d9603b786491fd1193f83e537676ea5
Hervé Zossou <hervezossou8@gmail.com>
2025-05-13 19:34:44 +0100

    feat: create IconLink component

---
+++ app/components/atoms/IconLink.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+import Link from "next/link";
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
+
+type Variant = "lime" | "dark" | "light";
+
+interface IconLinkProps {
+    variant: Variant;
+    label: string;
+    href: string;
+}
+
+const globalStyle = "w-auto h-[41px] flex items-center gap-[15px] text-xl font-normal p-0 group"
+const baseIconVariantStyle = "flex items-center justify-center w-[41px] h-[41px] rounded-full"
+const baseLabelStyle = "text-xl leading-[28px]"
+
+const iconVariantStyle: Record<Variant, string> = {
+    dark: "bg-dark text-lime stroke-lime group-hover:text-white group-hover:stroke-white",
+    light: "bg-white text-lime stroke-lime group-hover:text-dark group-hiver:stroke-dark",
+    lime: "bg-lime text-dark stroke-dark group-hover:text-white group-hover:stroke-white",
+}
+
+const labelVariantStyle: Record<Variant, string> = {
+    dark: "text-slate-900",
+    light: "text-white",
+    lime: "text-slate-900 group-hover:text-white",
+}
+
+export const IconLink = ({variant = "dark", label, href}: IconLinkProps) => {
+    return(
+        <Link className={clsx(globalStyle)} href={href}>
+            <div className={clsx(baseIconVariantStyle, iconVariantStyle[variant])}>
+                <ArrowUpRightIcon className="size-6 font-bold stroke-2" />
+            </div>
+            <span className={clsx(baseLabelStyle, labelVariantStyle[variant])}>{label}</span>
+        </Link>
+    )
+}
