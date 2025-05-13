eee32c0e719dc53d7dac9fc333767903fc2c9042
Hervé Zossou <hervezossou8@gmail.com>
2025-05-13 16:01:45 +0100

    Setting up colors, fonts and typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client"
+import { Typography } from "./components/atoms/Typography";
 
 export default function Page() {
   return (
-    <main>
-
+    <main className="p-8 max-w-screen max-h-screen">
+        <Typography variant="h1">
+          Bienvenue sur ma landing page
+        </Typography>
+        <Typography variant="p" className="mb-8">
+          Découvrez une solution rapide, moderne et adaptée à vos besoins.
+        </Typography>
     </main>
   );
 }
