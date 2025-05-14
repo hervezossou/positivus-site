610b45e24dde56637bb8720d0e5b89e7ae92fcb3
Hervé Zossou <hervezossou8@gmail.com>
2025-05-14 21:33:25 +0100

    added logos marquee images

---
+++ app/lib/data.ts
@@ -0,0 +1,8 @@
+export const logos = [
+    "/logos/amazon.png",
+    "/logos/dribbble.png",
+    "/logos/hubspot.png",
+    "/logos/netflix.png",
+    "/logos/notion.png",
+    "/logos/zoom.png"
+]
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client"
+
 import { HeroSection } from "./components/home/HeroSection";
+import { LogoMarquee } from "./components/home/LogoMarquee";
 
 export default function Page() {
   return (
-    <main className="mx-auto max-w-screen max-h-screen">
+    <main className="mx-auto space-y-4 max-w-full max-h-screen pt-10">
         <HeroSection />
+        <LogoMarquee />
     </main>
   );
 }
