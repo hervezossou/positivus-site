69a60fee964baf152e01adcb26a80e14ca8f5e09
Hervé Zossou <hervezossou8@gmail.com>
2025-05-16 19:37:44 +0100

    update: added box-sizing:border-box to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${spaceGrotesk.variable} antialiased`}
+        className={`${spaceGrotesk.variable} box-border antialiased`}
       >
         {children}
       </body>
