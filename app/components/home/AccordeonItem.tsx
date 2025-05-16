7e1bb397700e7ff77fdf1e90245e3bfd862f66f3
Hervé Zossou <hervezossou8@gmail.com>
2025-05-16 18:04:37 +0100

    feat: create FaqSection component

---
+++ app/components/home/AccordeonItem.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import Image from "next/image";
+import { Typography } from "../common/Typography";
+import { motion, AnimatePresence } from "motion/react";
+import { useMediaQuery } from "components/app/lib/hooks";
+
+interface AccordeonItemProps {
+    isOpen: boolean;
+    data: {
+        position: string;
+        title: string;
+        description: string;
+    };
+    onToggle: () => void;
+}
+
+export const AccordeonItem = ({ isOpen = false, data, onToggle }: AccordeonItemProps) => {
+    const isMobile = useMediaQuery('max-width: 768px');
+    return(
+        <div className={`${isOpen ? "bg-lime" : "bg-cloud"} w-full flex flex-col gap-[30px] px-15 py-[41px] rounded-45 border [filter:drop-shadow(0px_5px_0px_#191A23)]`}>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <Typography variant="h1">{data.position}</Typography>
+                    <Typography variant={`${isMobile ? "h4" : "h3"}`}>
+                        {data.title}
+                    </Typography>
+                </div>
+                <motion.div 
+                    className="flex items-center justify-center"
+                    animate={{ rotate: isOpen ? 180 : 0 }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                >
+                    {isOpen ? 
+                        <Image
+                            src="/icons/minus-icon.png" 
+                            width={58}
+                            height={58}
+                            alt="minus icon"
+                            className="size-12 text-white hover:cursor-pointer" 
+                            onClick={onToggle}
+                        /> : 
+                        <Image
+                            src="/icons/plus-icon.png"
+                            width={58}
+                            height={58}
+                            alt="plus icon"
+                            className="size-12 text-white hover:cursor-pointer"
+                            onClick={onToggle}
+                        />
+                    }
+                </motion.div>
+            </div>
+            <AnimatePresence initial={false}>
+                {isOpen && (
+                    <motion.div 
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: "auto", opacity: 1 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                        className="overflow-hidden"
+                    >
+                        <div className="flex flex-col items-center gap-2">
+                            <div className="bg-black w-full h-[1px] my-2"></div>
+                            <Typography variant="p">{data.description}</Typography>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
+    )
+}
---
+++ app/components/home/FaqSection.tsx
@@ -0,0 +1,42 @@
+import { Heading } from "../common/Heading"
+import { Typography } from "../common/Typography"
+import { useMediaQuery } from "components/app/lib/hooks"
+import { FaqData } from "components/app/lib/data"
+import { AccordeonItem } from "./AccordeonItem"
+import { useState } from "react"
+
+export const FaqSection = () => {
+    const isMobile = useMediaQuery('(max-width: 768px)');
+    const [openIndex, setOpenIndex] = useState(0); // the first accordeon is open by default
+
+    const handleToggle = (index: number) => {
+        setOpenIndex(index === openIndex ? -1 : index); // onClick on the opened accordeon to close it
+    };
+
+    return (
+        <section className="flex flex-col gap-y-14 px-5 py-4 lg:px-10 lg:py-10 xl:px-[110px]">
+            <div className="flex flex-col items-center justify-center gap-8 md:flex-row md:justify-start md:gap-10">
+                {isMobile ? 
+                (<div className="flex flex-col items-center gap-0">
+                    <Heading variant="lime" label="Our Working" />
+                    <Heading variant="lime" label="Process" />
+                </div>) : (
+                    <Heading variant="lime" label="Our Working Process" />
+                )}
+                <Typography variant="p" className="w-auto text-center md:w-[292px] md:text-left">
+                    Step-by-Step Guide to Achieving Your Business Goals
+                </Typography>
+            </div>
+            <div className="w-full flex flex-col items-center justify-between gap-y-4">
+                {FaqData.map((item, index) => (
+                    <AccordeonItem 
+                        isOpen={openIndex === index}
+                        key={`${item.title}-${index}`} 
+                        data={item}
+                        onToggle={() => handleToggle(index)}
+                    />
+                ))}
+            </div>
+        </section>
+    )
+}
