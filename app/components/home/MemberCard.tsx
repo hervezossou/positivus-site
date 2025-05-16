4e6749d2d53117058f37895308ec2499d4e57748
Hervé Zossou <hervezossou8@gmail.com>
2025-05-16 21:32:56 +0100

    feat: create TeamSection and MemberCard components

---
+++ app/components/home/MemberCard.tsx
@@ -0,0 +1,53 @@
+import { Typography } from "../common/Typography"
+import { members } from "components/app/lib/data"
+import Image from "next/image"
+import Link from "next/link"
+
+interface MemberCardProps {
+    data: typeof members[0];
+}
+
+export const MemberCard = ({ data } : MemberCardProps) => {
+    return (
+        <div className="w-full  bg-cloud flex flex-col gap-[30px] px-[35px] py-[40px] rounded-45 border [filter:drop-shadow(0px_5px_0px_#191A23)] hover:scale-105 duration-300 ease-in-out">
+            <div className="flex h-[102px] items-end justify-between space-x-3">
+                <div className="flex items-center justify-center w-[102.82px] h-[102.82px]">
+                    <Image 
+                        src={data.picture} 
+                        alt={`${data.name} picture`} 
+                        width={102}
+                        height={102}
+                        loading="lazy"
+                    />
+                </div>
+                <div className="flex flex-col items-end gap-4">
+                    <Link 
+                        href="https://www.linkedin.com/in/hervezossou" 
+                        target="_blank" 
+                        className="bg-black w-[34px] h-[34px] flex items-center justify-center rounded-full group hover:scale-110 duration-200 ease-in-out"
+                    >
+                        <Image 
+                            src={data.icon} 
+                            alt="linkedin icon" 
+                            width={17} 
+                            height={17} 
+                            className="duration-200 ease-in-out group-hover:scale-110"
+                        />
+                    </Link>
+                    <div className="flex w-full flex-col items-start">
+                        <Typography variant="h4">
+                            {data.name}
+                        </Typography>
+                        <Typography variant="p">
+                            {data.role}
+                        </Typography>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-black w-full h-[1px] my-2"></div>
+            <Typography variant="p" className="w-[317px]">
+                {data.description}
+            </Typography>
+        </div>
+    )
+}
---
+++ app/components/home/TeamSection.tsx
@@ -0,0 +1,33 @@
+import { members } from "components/app/lib/data"
+import { Heading } from "../common/Heading"
+import { Typography } from "../common/Typography"
+import { MemberCard } from "./MemberCard"
+import { Button } from "../common/Button"
+
+export const TeamSection = () => {
+    return (
+        <section className="flex flex-col gap-y-10 px-5 py-4 lg:px-10 lg:py-10 xl:px-[110px]">
+            <div className="flex flex-col items-center justify-center gap-8 md:flex-row md:justify-start md:gap-10">
+                <Heading variant="lime" label="Team" />
+                <Typography variant="p" className="w-auto text-center md:w-[473px] md:text-left">
+                    Meet the skilled and experienced team behind our successful digital marketing strategies
+                </Typography>
+            </div>
+            <div className="grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-8">
+                {members.map((person, index) => (
+                    <MemberCard 
+                        key={`${person.name}-${index}`} 
+                        data={person} 
+                    />
+                ))}
+            </div>
+            <div className="flex justify-end">
+                <Button 
+                    variant="contained" 
+                    label="See all of them" 
+                    onClick={() => alert("You get all the team members !")} 
+                />
+            </div>
+        </section>
+    )
+}
