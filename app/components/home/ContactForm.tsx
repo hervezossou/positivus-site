82990c57a8c54d5e2e0aefe5e20ef614ad5131e0
Hervé Zossou <hervezossou8@gmail.com>
2025-05-20 15:12:32 +0100

    feat: added ContactFom and ContactSection components

---
+++ app/components/home/ContactForm.tsx
@@ -0,0 +1,107 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "../common/Button"
+
+export const ContactForm = () => {
+    const [isChecked, setIsChecked] = useState(true);
+
+    const handleClick = (e: React.FormEvent) => {
+        e.preventDefault();
+        alert("You sent a message")
+    }
+    
+    return (
+        <form 
+            action="" 
+            className="w-full flex flex-col gap-y-10 px-4 py-5 lg:w-3/5"
+        >
+            <div className="flex items-center space-x-8">
+                <div className="flex items-center space-x-2">
+                    <input 
+                        type="radio" 
+                        name="greeting" 
+                        id="greeting-hi"
+                        className="custom-radio"
+                        checked={isChecked}
+                        onChange={() => setIsChecked(!isChecked)}
+                    />
+                    <label 
+                        htmlFor="greeting-hi" 
+                        className="cursor-pointer text-base text-black"
+                    >
+                        Say Hi
+                    </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <input 
+                        type="radio" 
+                        name="greeting" 
+                        id="greeting-quote"
+                        className="custom-radio" 
+                        checked={!isChecked}
+                        onChange={() => setIsChecked(!isChecked)}
+                    />
+                    <label 
+                        htmlFor="greeting-quote" 
+                        className="cursor-pointer text-base text-black"
+                    >
+                        Get a Quote
+                    </label>
+                </div>
+            </div>
+            <div className="flex flex-col items-start justify-between gap-y-[25px]">
+                <div className="w-full flex items-start flex-col space-y-2">
+                    <label 
+                        htmlFor="fullname" 
+                        className="text-base text-black"
+                    >
+                        Name
+                    </label>
+                    <input 
+                        type="text" 
+                        name="fullname" 
+                        id="fullname"
+                        placeholder="Name"
+                        className="border px-[30px] py-[18px] rounded-[14px] bg-white w-full" 
+                    />
+                </div>
+                <div className="w-full flex items-start flex-col space-y-2">
+                    <label 
+                        htmlFor="email" 
+                        className="text-base text-black"
+                    >
+                        Email
+                    </label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email"
+                        placeholder="Email"
+                        className="border px-[30px] py-[18px] rounded-[14px] bg-white w-full" 
+                    />
+                </div>
+                <div className="w-full flex items-start flex-col space-y-2">
+                    <label 
+                        htmlFor="message" 
+                        className="text-base text-black"
+                    >
+                        Message
+                    </label>
+                    <textarea 
+                        name="message" 
+                        id="message"
+                        placeholder="Message"
+                        rows={6}
+                        className="border px-[30px] py-[18px] rounded-[14px] bg-white w-full" 
+                    />
+                </div>
+            </div>
+            <Button 
+                variant="contained" 
+                label="Send Message"
+                onClick={handleClick}
+            />
+        </form>
+    )
+}
---
+++ app/components/home/ContactSection.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { ContactForm } from "./ContactForm";
+import { Heading } from "../common/Heading";
+import { Typography } from "../common/Typography";
+import Image from "next/image";
+
+export const ContactSection = () => {
+    return (
+        <section className="flex flex-col gap-y-10 px-5 py-4 lg:px-10 lg:py-10 xl:px-[110px]">
+            <div className="flex flex-col items-center justify-center gap-8 md:flex-row md:justify-start md:gap-10">
+                <Heading variant="lime" label="Contact Us" />
+                <Typography variant="p" className="max-w-[390px] text-center md:w-[473px] md:text-left">
+                    Connect with Us: Let's Discuss Your Digital Marketing Needs
+                </Typography>
+            </div>
+            <div className={`bg-cloud rounded-45 flex items-center px-5 py-10 h-full lg:justify-between lg:gap-x-6`}>
+                <ContactForm />
+                <div className="hidden translate-x-73 lg:h-screen lg:flex lg:items-center lg:rounded-3xl">
+                    <Image 
+                        src="/images/star-sun.png" 
+                        width={692} 
+                        height={648} 
+                        alt="illustration" 
+                        className="object-cover scale-110"
+                    />
+                </div>
+            </div>
+        </section>
+    )
+}
