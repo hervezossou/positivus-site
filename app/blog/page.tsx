b147132831f1de01eadd3307bdce5e4abf512a4e
Hervé Zossou <hervezossou8@gmail.com>
2025-06-04 18:34:02 +0100

    update(blog): remove unused vars

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-import clsx from "clsx";
-import { useState } from "react";
 import { HeroSection } from "../components/blog/sections/HeroSection";
 import { ArticleHeading } from "../components/blog/sections/ArticleHeading";
 
