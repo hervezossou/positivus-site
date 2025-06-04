8b43f0213a5f24be0564d5e08d8672f58090053c
Hervé Zossou <hervezossou8@gmail.com>
2025-06-04 18:16:29 +0100

    update(blog): added ArticleHeading component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,15 @@
-import { HeroSection } from "../components/blog/sections/HeroSection";
-import { PostAuthorCard } from "../components/blog/ui/PostAuthorCard";
+"use client"
 
-export default function Page() {
+import clsx from "clsx";
+import { useState } from "react";
+import { HeroSection } from "../components/blog/sections/HeroSection";
+import { ArticleHeading } from "../components/blog/sections/ArticleHeading";
+
+export default function Page() { 
     return (
         <>
             <HeroSection />
-            <PostAuthorCard />
+            <ArticleHeading />
         </>
     )
 }
