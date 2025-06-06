f61499d9f19377fb77878eea0d2f320b68647324
Hervé Zossou <hervezossou8@gmail.com>
2025-06-06 15:44:28 +0100

    feat(blog): added ArticleText component in page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,12 +2,14 @@
 
 import { HeroSection } from "../components/blog/sections/HeroSection";
 import { ArticleHeading } from "../components/blog/sections/ArticleHeading";
+import { ArticleText } from "../components/blog/sections/ArticleText";
 
 export default function Page() { 
     return (
         <>
             <HeroSection />
             <ArticleHeading />
+            <ArticleText />
         </>
     )
 }
