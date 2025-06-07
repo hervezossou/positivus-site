b8af664abad68a752d0300b3f3cd4d48cbe85b86
Hervé Zossou <hervezossou8@gmail.com>
2025-06-07 08:29:50 +0100

    update(fontsize): update font size for a more intuitive interface

--- app/lib/utils/fontsize.ts
+++ app/lib/utils/fontsize.ts
@@ -2,15 +2,15 @@
 import { FontSize } from "./types"
 
 export const paragraphSize: Record<FontSize, string> = {
-  small: "text-sm",
+  small: "text-sm md:text-base",
   medium: "text-base md:text-lg",
   large: "text-lg md:text-xl",
 }
 
 export const titleSize: Record<FontSize, string> = {
-  small: "text-lg",
-  medium: "text-xl md:text-2xl",
-  large: "text-2xl md:text-3xl",
+  small: "text-lg md:text-xl",
+  medium: "text-2xl md:text-3xl",
+  large: "text-3xl md:text-4xl",
 }
 
 export const hightlightSize: Record<FontSize, string> = {
