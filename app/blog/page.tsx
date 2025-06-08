56f0f2b6c7c502157aebb088c34b69bb93e3ea82
Hervé Zossou <hervezossou8@gmail.com>
2025-06-08 10:49:27 +0100

    update(blogPage): added star icon between articleText and MoreArticles sections

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,7 @@ import { HeroSection } from "../components/blog/sections/HeroSection";
 import { ArticleHeading } from "../components/blog/sections/ArticleHeading";
 import { ArticleText } from "../components/blog/sections/ArticleText";
 import { MoreArticles } from "@/blog/sections/MoreArticles";
+import Image from "next/image";
 
 export default function Page() { 
     return (
@@ -11,6 +12,13 @@ export default function Page() {
             <HeroSection />
             <ArticleHeading />
             <ArticleText />
+            <Image 
+                src="/icons/star.png" 
+                width={24} 
+                height={24} 
+                alt="Logo star"
+                className="flex items-center justify-between mx-auto py-8"
+            />
             <MoreArticles />
         </>
     )
