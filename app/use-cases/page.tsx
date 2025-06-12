6eef417c1503b0d73dfd47bff13c0c527dad1d52
Hervé Zossou <hervezossou8@gmail.com>
2025-06-12 21:52:31 +0100

    update(use-cases): nested HeroSection component

--- app/use-cases/page.tsx
+++ app/use-cases/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { HeroSection } from "@/use-cases/sections/HeroSection"
 import { FeaturedStory } from "@/use-cases/sections/FeaturedStory"
 import { SuccessStories } from "@/use-cases/sections/SuccessStories"
 import { CallToAction } from "@/use-cases/sections/CallToAction"
@@ -8,6 +9,7 @@ import { TestimonialSection } from "@/home/sections/TestimonialSection"
 export default function Page() {
     return (
         <>
+            <HeroSection />
             <FeaturedStory />
             <SuccessStories />
             <TestimonialSection />
