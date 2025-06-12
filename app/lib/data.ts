a05e2721dc755fd071942b32e0b660907c018e4d
Hervé Zossou <hervezossou8@gmail.com>
2025-06-12 18:06:08 +0100

    update(data): added successStories data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -319,6 +319,33 @@ export const moreInsights = [
     },
 ]
 
+export const successStories = [
+    {
+        title: "E-commerce Fashion Brand",
+        description: "We implemented a tailored SEO strategy that boosted organic search rankings, leading to a 150% increase in website traffic and a 50% rise in online sales within six months.",
+    },
+    {
+        title: "Local Restaurant Chain",
+        description: "By optimizing local SEO and enhancing Google Mu Business profiles, we increased visibility, driving a 300% boost in online reservations and foot traffic.",
+    },
+    {
+        title: "Healthcare Provider",
+        description: "Through content optimization and targeted keyword strategy, we helped a healthcare provider rank on the first page for critical services, resulting in a 180% increase in inquiries and patient bookings.",
+    },
+    {
+        title: "E-commerce Fashion Brand",
+        description: "We implemented a tailored SEO strategy that boosted organic search rankings, leading to a 150% increase in website traffic and a 50% rise in online sales within six months.",
+    },
+    {
+        title: "Local Restaurant Chain",
+        description: "By optimizing local SEO and enhancing Google Mu Business profiles, we increased visibility, driving a 300% boost in online reservations and foot traffic.",
+    },
+    {
+        title: "Healthcare Provider",
+        description: "Through content optimization and targeted keyword strategy, we helped a healthcare provider rank on the first page for critical services, resulting in a 180% increase in inquiries and patient bookings.",
+    },
+]
+
 export const featuredStory = [
     {
         title: "200%",
