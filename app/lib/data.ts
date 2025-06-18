2b593b6ce984d82876edf4415f7e845263ca524a
Hervé Zossou <hervezossou8@gmail.com>
2025-06-18 20:57:54 +0100

    feat(data.ts): added work process data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -364,3 +364,36 @@ export const featuredStory = [
         description: "improvement in conversion rate"
     }
 ]
+
+export const workProcess = [
+    {
+        order: "01",
+        title: "Website Audit & Analysis",
+        description: "We begin by conducting a comprehensive audit to identify areas for improvement, including technical issues, content quality, and keyword opportunities."
+    },
+    {
+        order: "02",
+        title: "Keyword Research & Strategy",
+        description: "Our team researches the most relevant and high-impact keywords for your business, ensuring that we target terms that drive qualified traffic."
+    },
+    {
+        order: "03",
+        title: "On-Page Optimization",
+        description: "We optimize your website's structure, meta tags, and content to make it search engine-friendly, improving your visibility and relevance."
+    },
+    {
+        order: "04",
+        title: "Content Creation & Optimization",
+        description: "Hight-quality content is key to SEO. We create or refine content that engages your audience while aligning with search engine algorithms."
+    },
+    {
+        order: "05",
+        title: "Link Building",
+        description: "We develop a link-building strategy to acquire authoritative, relevant backlinks that boost your domain authority and search rankings."
+    },
+    {
+        order: "06",
+        title: "Monitoring & Reporting",
+        description: "We continuously monitor performance, track rankings, and provide detailed reports, making data-driven adjustments to maintain and improve your results."
+    }
+]
