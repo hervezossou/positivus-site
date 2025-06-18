452f540f8dfe84ff4b86e3fe8f29d7953c4df258
Hervé Zossou <hervezossou8@gmail.com>
2025-06-18 16:42:06 +0100

    update(services): added boost rankin section

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/services/sections/HeroSection"
+import { BoostRanking } from "@/services/sections/BoostRanking"
 
 export default function Page() {
     return (
         <>
             <HeroSection />
+            <BoostRanking />
         </>
     )
 }
