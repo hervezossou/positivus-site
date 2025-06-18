a2e0c0e0db304f63fcb25050184cca8f34d2bacc
Hervé Zossou <hervezossou8@gmail.com>
2025-06-18 20:58:35 +0100

    update(services): added WorkProcess component to service page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "@/services/sections/HeroSection"
 import { BoostRanking } from "@/services/sections/BoostRanking"
+import { WorkProcess } from "@/services/sections/WorkProcess"
 
 export default function Page() {
     return (
         <>
             <HeroSection />
             <BoostRanking />
+            <WorkProcess />
         </>
     )
 }
