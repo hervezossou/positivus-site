a46c7e2c978c4c49033e68c7fcde7b97638ff85e
Hervé Zossou <hervezossou8@gmail.com>
2025-06-19 16:18:27 +0100

    feat(servicesPage): added SearchRanking section

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,6 +3,7 @@ import { BoostRanking } from "@/services/sections/BoostRanking"
 import { WorkProcess } from "@/services/sections/WorkProcess"
 import { UseCaseSection } from "@/services/sections/UseCaseSection"
 import { ServiceSection } from "@/services/sections/ServiceSection"
+import { SearchRanking } from "@/services/sections/SearchRanking"
 
 export default function Page() {
     return (
@@ -12,6 +13,7 @@ export default function Page() {
             <WorkProcess />
             <UseCaseSection />
             <ServiceSection />
+            <SearchRanking />
         </>
     )
 }
