9e08185f3b68ae72b63db9962a4c0ff8d591b41d
Hervé Zossou <hervezossou8@gmail.com>
2025-06-19 13:48:40 +0100

    feat(data.ts): added use cases data and update services page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -397,3 +397,18 @@ export const workProcess = [
         description: "We continuously monitor performance, track rankings, and provide detailed reports, making data-driven adjustments to maintain and improve your results."
     }
 ]
+
+export const useCasesData = [
+    {
+        title: "E-commerce Fashion Brand",
+        description: "We implemented a tailored SEO strategy that boosted organic search rankings, leading to a 150% increase in website traffic and a 50% rise in online sales within six months."
+    },
+    {
+        title: "Local Restaurant Chain",
+        description: "By optimizing local SEO and enhancing Google My Business profiles, we increase visibility, driving a 300% boost in online reservations and  foot traffic."
+    },
+    {
+        title: "Healthcare Provider",
+        description: "Through content optimization and targeted keyword strategy, we helped a healthcare provider rank on the first page for critical services, resulting in a 180% increase in inquiries and patient bookings."
+    }
+]
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "@/services/sections/HeroSection"
 import { BoostRanking } from "@/services/sections/BoostRanking"
 import { WorkProcess } from "@/services/sections/WorkProcess"
+import { UseCaseSection } from "@/services/sections/UseCaseSection"
 
 export default function Page() {
     return (
@@ -8,6 +9,7 @@ export default function Page() {
             <HeroSection />
             <BoostRanking />
             <WorkProcess />
+            <UseCaseSection />
         </>
     )
 }
