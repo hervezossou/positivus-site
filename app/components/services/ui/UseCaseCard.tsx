981d42696351d7270f9fbee91eb29804868a03ec
Hervé Zossou <hervezossou8@gmail.com>
2025-06-19 13:47:59 +0100

    feat(UseCaseCard): create UseCaseCard component

---
+++ app/components/services/ui/UseCaseCard.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import { Typography } from "@/common/Typography"
+import { TextLink } from "@/common/TextLink"
+
+type Variant = "mobile" | "desktop"
+
+interface UseCaseCardProps {
+    title: string;
+    description: string;
+    goTo: string;
+    className?: string
+}
+
+const baseStyle = "w-96 h-72 bg-black rounded-4xl p-8 flex shrink-0 flex-col items-start justify-between gap-6 px-10 xl:h-[316px] xl:pl-2 xl:pr-8 xl:py-0 xl:bg-transparent"
+
+export const UseCaseCard = ({ title, description, goTo, className } : UseCaseCardProps) => {
+    return (
+        <div className={clsx(baseStyle, className)}>
+            <Typography variant="h3" className="text-white">
+                {title}
+            </Typography>
+            <Typography variant="p" className="text-gray-300">
+                {description}
+            </Typography> 
+            <TextLink 
+                variant="lime" 
+                label="Learn more" 
+                href={goTo} 
+            />
+        </div>
+    )
+}
