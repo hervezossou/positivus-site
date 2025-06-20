44d5b7f05b6dcf343e604ab01e4df748e85e9a80
Hervé Zossou <hervezossou8@gmail.com>
2025-06-20 16:38:43 +0100

    update(AboutPage): added CoreValuesSection

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import { AboutHeroSection } from "@/about/sections/AboutHeroSection"
 import { ImpactNumbersSection } from "@/about/sections/ImpactNumbersSection"
+import { CoreValueSection } from "@/about/sections/CoreValueSection"
 
 export default function Page() {
     return (
         <>
             <AboutHeroSection />
             <ImpactNumbersSection />
+            <CoreValueSection />
         </>
     )
 }
