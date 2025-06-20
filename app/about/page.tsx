dfdd341c25dac5ce9172d6ed0888c8ec045cfeed
Hervé Zossou <hervezossou8@gmail.com>
2025-06-20 16:01:28 +0100

    update(AboutPage): added ImpactNumbersSection

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 import { AboutHeroSection } from "@/about/sections/AboutHeroSection"
+import { ImpactNumbersSection } from "@/about/sections/ImpactNumbersSection"
 
 export default function Page() {
     return (
         <>
             <AboutHeroSection />
+            <ImpactNumbersSection />
         </>
     )
 }
