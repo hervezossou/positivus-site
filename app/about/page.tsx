a5d44c10b1adba2f037835e44ab8f33857b54bfb
Hervé Zossou <hervezossou8@gmail.com>
2025-06-20 12:01:12 +0100

    feat(AboutPage): added AboutHeroSection

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,9 @@
+import { AboutHeroSection } from "@/about/sections/AboutHeroSection"
+
 export default function Page() {
     return (
-        <main>
-            <h1>About Page</h1>
-        </main>
+        <>
+            <AboutHeroSection />
+        </>
     )
 }
