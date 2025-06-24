3a9526f5c88f6d5e00e7de95c42dc2c50d858143
Hervé Zossou <hervezossou8@gmail.com>
2025-06-24 11:38:02 +0100

    update(AboutPage): added AwardSection

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import { AboutHeroSection } from "@/about/sections/AboutHeroSection"
 import { ImpactNumbersSection } from "@/about/sections/ImpactNumbersSection"
 import { CoreValueSection } from "@/about/sections/CoreValueSection"
+import { AwardSection } from "@/about/sections/AwardSection"
 
 export default function Page() {
     return (
@@ -8,6 +9,7 @@ export default function Page() {
             <AboutHeroSection />
             <ImpactNumbersSection />
             <CoreValueSection />
+            <AwardSection />
         </>
     )
 }
