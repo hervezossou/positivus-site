d2e7353bc1385e178e7c6bc8f9cddc8b1907ca02
Hervé Zossou <hervezossou8@gmail.com>
2025-06-24 11:40:44 +0100

    feat(AwardSection): create AwardSection component

---
+++ app/components/about/sections/AwardSection.tsx
@@ -0,0 +1,30 @@
+
+import { Heading } from "@/common/Heading";
+import { Typography } from "@/common/Typography";
+import { AwardCard } from "../ui/AwardCard";
+import { awards } from "../../../lib/data";
+
+export const AwardSection = () => {
+    return (
+        <section className="py-4 lg:py-10">
+            <div className="flex flex-col items-start justify-between gap-8 lg:gap-28">
+                <div className="w-full flex flex-col items-center justify-between gap-8 xl:flex-row xl:items-start xl:justify-start xl:gap-12">
+                    <Heading variant="lime" label="Awards & Recognition" />
+                    <Typography variant="p" className="max-w-xl">
+                        At Positivus, our commitment to excellence has been recognized by industry leaders. Here are some  of our proudest achievements:
+                    </Typography>
+                </div>
+                <div className="w-full grid grid-cols-2 items-start justify-between gap-4 xl:grid-cols-4 xl:gap-8">
+                    {awards.map((award, index) => (
+                        <AwardCard 
+                            key={`${award.awardTitle}-${index}`}
+                            imageUrl={award.imageUrl} 
+                            awardTitle={award.awardTitle} 
+                            awardDescription={award.awardDescription} 
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
