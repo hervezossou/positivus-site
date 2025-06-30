6f38260fcc158024063acbf4e505359eb7563bff
Hervé Zossou <hervezossou8@gmail.com>
2025-06-30 16:56:40 +0100

    update(AwardSection): fix text align to flex-col and center on small device & flex-row and items-start for larger devices

--- app/components/about/sections/AwardSection.tsx
+++ app/components/about/sections/AwardSection.tsx
@@ -8,7 +8,7 @@ export const AwardSection = () => {
     return (
         <section className="py-4 lg:py-10">
             <div className="flex flex-col items-start justify-between gap-8 lg:gap-28">
-                <div className="w-full flex flex-col items-center justify-between gap-8 xl:flex-row xl:items-start xl:justify-start xl:gap-12">
+                <div className="w-full flex flex-col items-center justify-between gap-8 lg:flex-row lg:items-start lg:justify-start lg:gap-12">
                     <Heading variant="lime" label="Awards & Recognition" />
                     <Typography variant="p" className="max-w-xl">
                         At Positivus, our commitment to excellence has been recognized by industry leaders. Here are some  of our proudest achievements:
