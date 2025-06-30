c756288d1b3d0f5247be747886aa57e534f14d46
Hervé Zossou <hervezossou8@gmail.com>
2025-06-30 16:44:40 +0100

    update(AboutHeroSection): resolve flex align

--- app/components/about/sections/AboutHeroSection.tsx
+++ app/components/about/sections/AboutHeroSection.tsx
@@ -5,8 +5,8 @@ export const AboutHeroSection = () => {
     return (
         <section className="py-4 lg:py-10">
             <div className="flex flex-col items-start justify-between gap-8 lg:flex-row lg:items-center lg:bg-cloud lg:p-8 lg:rounded-45">
-                <div className="w-full flex flex-col items-start justify-between gap-4 lg:w-1/2">
-                    <Typography variant="h1" className="block max-w-lg leading-16 lg:hidden">
+                <div className="w-full flex flex-col items-center justify-between gap-4 lg:w-1/2 lg:items-start">
+                    <Typography variant="h1" className="block max-w-2xl leading-16 lg:hidden">
                         Together for Success
                     </Typography>
                     <Image 
@@ -20,7 +20,7 @@ export const AboutHeroSection = () => {
                     <Typography variant="h1" className="hidden lg:block lg:max-w-lg lg:leading-16">
                         Together for Success
                     </Typography>
-                    <Typography variant="p" className="text-justify max-w-lg lg:max-w-md">
+                    <Typography variant="p" className="text-justify mx-auto max-w-lg lg:max-w-md">
                         At Positivus, we help businesses grow by combining creativity, innovation,, and data-driven strategies. Together, we build a future  of shared  success.
                     </Typography>
                 </div>
